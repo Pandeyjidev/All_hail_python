11bc547b7a4db36747f37f5351f155d89597d277
Utkarsh Pandey <pandeyji.developer@gmail.com>
2018-03-16 16:50:27 -0700

    filters,columns,duplicate

---
+++ Python_Specifics/check_columns.py
@@ -0,0 +1,10 @@
+def column(matrix, i):
+    return [row[i] for row in matrix]
+
+def column2(matrix):
+    i=0
+    for row[i] in matrix:
+        print(row[0])
+        i +=1
+
+column2([[1,2,3],[4,5,6],[7,8,9]])
---
+++ Python_Specifics/duplicate_in_a_list.py
@@ -0,0 +1,3 @@
+def duplicate(arr):
+    a=set(arr)
+    return len(a)==len(arr)
