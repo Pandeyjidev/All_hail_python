e3f99ca601da994f61ac007efea52e4cec23f238
Utkarsh Pandey <pandeyji.developer@gmail.com>
2018-03-19 22:30:05 -0700

    few more

--- Leetcode/266_Palindrome_Permutation.py
+++ Leetcode/266_Palindrome_Permutation.py
@@ -27,5 +27,39 @@ def perm(s):
             for p in perm(xs):
                 yield [x] + p
 
+def permute_palin_brute(s):
+    count = 0
+    for i in range(128):
+        c=0
+        if(count > 1):
+            break
+        for i in range(len(s)):
+            if ord(s[i]) == i:
+                c +=1
+        count +=c%2
+    return count<=1
 
-palindrom_perm("aab")
+def permute_hashmap(s):
+    count = {}
+    for ss in s:
+        if ss in count:
+            count[ss] += 1
+        else:
+            count[ss] = 1 
+    cnt = 0
+    for c in count:
+        if count[c]%2 == 1:
+            cnt +=1
+    return cnt<=1    
+
+#  DOESN't Work
+# def perm_set(s):
+#     s_set = set(s)
+#     return len(s)%len(s_set) <=1
+
+# print(perm_set("aaabbccccxxsd"))
+
+
+# print(permute_hashmap("abaab"))
+# print(permute_palin_brute("abaab") )
+# palindrom_perm("aab")
