947d47372ae83688afa9e842ab392271a2735e64
Utkarsh Pandey <pandeyji.developer@gmail.com>
2018-03-19 22:30:43 -0700

    few more

---
+++ Leetcode_Chapters/Strings/Implement_StrStr.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/885/
+# Implement strStr().
+
+# Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+# Example 1:
+
+# Input: haystack = "hello", needle = "ll"
+# Output: 2
+# Example 2:
+
+# Input: haystack = "aaaaa", needle = "bba"
+# Output: -1
+
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        # print(haystack)
+        # print(needle)
+        try:
+            idx = haystack.index(needle)
+        except:
+            idx = -1
+        # print(idx)
+        return idx
