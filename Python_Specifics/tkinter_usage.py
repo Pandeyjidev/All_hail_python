8065862d88e283b43953136aa6d3d2fccc629be4
Utkarsh Pandey <pandeyji.developer@gmail.com>
2018-03-26 20:20:47 -0700

    tkinter

---
+++ Python_Specifics/tkinter_usage.py
@@ -0,0 +1,31 @@
+import Tkinter as tk
+
+
+# Create instance
+win = tk.Tk()   
+
+# Add a title       
+win.title("Python GUI")
+
+#======================
+# functions
+#======================
+def get_current_windows_size():
+    win.update()                        # to get runtime size
+    print('width  =', win.winfo_width())
+    print('height =', win.winfo_height())
+
+def increase_window_width():
+    # min width can not be resized to less
+    # default heigth can be
+    win.minsize(width=300, height=1)    # 1 = default
+
+#======================
+# Start GUI
+#======================
+get_current_windows_size()
+increase_window_width()
+print()
+get_current_windows_size()
+
+win.mainloop()
